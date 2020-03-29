3b53d8b6b6f8398b4f2db8720d858d51bcb0e5ba
Jim Tyhurst <jim@tyhurst.com>
2020-03-29 09:35:33 -0700

    JHU CSSE changed file names and no longer shows state data for the US.

--- R/read_data.R
+++ R/read_data.R
@@ -11,14 +11,14 @@ library(tidyselect)
 #' Reads cumulative total of COVID-19 confirmed cases by day from a public dataset published by Johns Hopkins University (JHU).
 #'
 #' @details
-#' * Reads a [JHU CSSE](https://github.com/CSSEGISandData/COVID-19) data file, [time_series_19-covid-Confirmed.csv](https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv) of confirmed [COVID-19](https://www.who.int/emergencies/diseases/novel-coronavirus-2019) cases.
+#' * Reads a [JHU CSSE](https://github.com/CSSEGISandData/COVID-19) data file, [time_series_covid19_confirmed_global.csv](https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv) of confirmed [COVID-19](https://www.who.int/emergencies/diseases/novel-coronavirus-2019) cases.
 #' * Reformats data as a [tidy dataset](https://r4ds.had.co.nz/tidy-data.html).
 #' * Reformats the column names to [snake case](https://en.wikipedia.org/wiki/Snake_case).
 #'
-#' @return `tbl` with 6 columns: `country_region`, `province_state`, `lat`, `long`, `date`, `cumulative_total`. Most of the country data, except for China and United States, is not sub-divided by `province_state`, so that column has many `NA` values.
+#' @return `tbl` with 6 columns: `country_region`, `province_state`, `lat`, `long`, `date`, `cumulative_total`. Note: The country data for the United States and most other countries is _not_ sub-divided by `province_state`, so that column has many `NA` values.
 #'
 #' @export
-read_confirmed_cases <- function() {
+read_confirmed_cases_jhu_csse <- function() {
   # The source file is "wide" with one column per day.
   # Those date column labels look like:
   #   2/1/20  i.e. mm/dd/yy = Feb 1, 2020
@@ -27,7 +27,7 @@ read_confirmed_cases <- function() {
   # We pivot the table to make it "long" with one
   # observation per row.
   confirmed_cases <- readr::read_csv(
-    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
     ) %>%
     tidyr::pivot_longer(
       cols = tidyselect::matches(date_as_column_name_format),
@@ -55,14 +55,14 @@ read_confirmed_cases <- function() {
 #' Reads cumulative total of COVID-19 deaths by day from a public dataset published by Johns Hopkins University (JHU).
 #'
 #' @details
-#' * Reads a [JHU CSSE](https://github.com/CSSEGISandData/COVID-19) data file, [time_series_19-covid-Deaths.csv](https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv), of confirmed [COVID-19](https://www.who.int/emergencies/diseases/novel-coronavirus-2019) cases.
+#' * Reads a [JHU CSSE](https://github.com/CSSEGISandData/COVID-19) data file, [time_series_19-covid-Deaths.csv](https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv), of confirmed [COVID-19](https://www.who.int/emergencies/diseases/novel-coronavirus-2019) cases.
 #' * Reformats data as a [tidy dataset](https://r4ds.had.co.nz/tidy-data.html).
 #' * Reformats the column names to [snake case](https://en.wikipedia.org/wiki/Snake_case).
 #'
-#' @return `tbl` with 6 columns: `country_region`, `province_state`, `lat`, `long`, `date`, `cumulative_total`. Most of the country data, except for China and United States, is not sub-divided by `province_state`, so that column has many `NA` values.
+#' @return `tbl` with 6 columns: `country_region`, `province_state`, `lat`, `long`, `date`, `cumulative_total`. Note: The country data for the United States and most other countries is _not_ sub-divided by `province_state`, so that column has many `NA` values.
 #'
 #' @export
-read_deaths <- function() {
+read_deaths_jhu_csse <- function() {
   # The source file is "wide" with one column per day.
   # Those date column labels look like:
   #   2/1/20  i.e. mm/dd/yy = Feb 1, 2020
@@ -71,7 +71,7 @@ read_deaths <- function() {
   # We pivot the table to make it "long" with one
   # observation per row.
   deaths <- readr::read_csv(
-    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
+    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
   ) %>%
     tidyr::pivot_longer(
       cols = tidyselect::matches(date_as_column_name_format),
